53bf07b87d8016a0f057afb8e1694f653946872a
Volodya62013 <130237655+Volodya62013@users.noreply.github.com>
2025-05-21 21:36:08 +0700

    Add files via upload

---
+++ versions/desktop-version/pages/practice/tasks/task3/script.js
@@ -0,0 +1,24 @@
+function onCheck() {
+    let l1 = document.getElementById("l1");
+    let l2 = document.getElementById("l2");
+    let l3 = document.getElementById("l3");
+    let l4 = document.getElementById("l4");
+    let l5 = document.getElementById("l5");
+    let l6 = document.getElementById("l6");
+    let l7 = document.getElementById("l7");
+    let l8 = document.getElementById("l8");
+    let l9 = document.getElementById("l9");
+
+    let output = document.getElementById("outputik");
+
+    if(l1.value == 'print("Hello World!")' && l2.value == 'print("I love python!")') {
+        output.style = "color: greenyellow";
+        output.innerHTML = "Правильно!";
+    } else if(l1.value == "print('Hello World!')" && l2.value == "print('I love python!')") {
+        output.style = "color: greenyellow";
+        output.innerHTML = "Правильно!";
+    } else {
+        output.style = "color: red";
+        output.innerHTML = "Неправильно!";
+    }
+}
---
+++ versions/desktop-version/pages/practice/tasks/task4/script.js
@@ -0,0 +1,21 @@
+function onCheck() {
+    let l1 = document.getElementById("l1");
+    let l2 = document.getElementById("l2");
+    let l3 = document.getElementById("l3");
+    let l4 = document.getElementById("l4");
+    let l5 = document.getElementById("l5");
+    let l6 = document.getElementById("l6");
+    let l7 = document.getElementById("l7");
+    let l8 = document.getElementById("l8");
+    let l9 = document.getElementById("l9");
+
+    let output = document.getElementById("outputik");
+
+    if(l1.value == 'spisok = [5, 10, 2000]') {
+        output.style = "color: greenyellow";
+        output.innerHTML = "Правильно!";
+    } else {
+        output.style = "color: red";
+        output.innerHTML = "Неправильно!";
+    }
+}
